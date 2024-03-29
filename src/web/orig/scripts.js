41586457d4959955bb48f9a043a831bf1a89c65f
John Atwood <JohnDoeKyrgyz@gmail.com>
2024-03-28 20:36:51 -0400

    Updating json library

--- src/web/orig/scripts.js
+++ src/web/orig/scripts.js
@@ -2,7 +2,7 @@ const SETTING_BASIC = 0;
 const SETTING_ADVANCED = 100;
 const SETTING_EXPERT = 200;
 
-var url = "ws://" + window.location.hostname + ":1337/";
+const url = "ws://" + window.location.hostname + ":1337/";
 
 // This is called when the page finishes loading
 function init() {
@@ -17,13 +17,15 @@ function wsConnect(url) {
   // Connect to WebSocket server
   //console.log("try to connect to " + url);    
   //document.getElementById("TxtConn").innerHTML ="try to connect to " + url;
-  websocket = new WebSocket(url);
+  let websocket = new WebSocket(url);
   
   // Assign callbacks
-  websocket.onopen = function(evt) { onOpen(evt) };
-  websocket.onclose = function(evt) { onClose(evt) };
-  websocket.onmessage = function(evt) { onMessage(evt) };
-  websocket.onerror = function(evt) { onError(evt) };
+  websocket.onopen = evt => { onOpen(evt) };
+  websocket.onclose = evt => { onClose(evt) };
+  websocket.onmessage = evt => { onMessage(evt) };
+  websocket.onerror = evt => { onError(evt) };
+
+  window.websocket = websocket;
 }
 
 // Called when a WebSocket connection is established with the server
@@ -38,12 +40,11 @@ function onOpen(evt) {
 
 // Called when the WebSocket connection is closed
 function onClose(evt) {
-
   // Log disconnection state
   console.log("Disconnected");
   //document.getElementById("TxtConn").innerHTML ="disconnected";
   // Try to reconnect after a few seconds
-  setTimeout(function() { wsConnect(url) }, 2000);
+  setTimeout(() => { wsConnect(url) }, 2000);
 }
 
 function callMainPage(){
@@ -51,19 +52,19 @@ function callMainPage(){
 }
 
 function FntIdDec2Hex(value){
-  var retVal = "";
-  if (value != 0){
+  let retVal = "";
+  if (value !== 0){
     retVal = (value).toString(16).toUpperCase().padStart(2, '0')
     console.log("value="+value+",l="+retVal.length+"retval=" + retVal);
     while (retVal.length < 6){
       retVal = "0" + retVal;
-    };
+    }
   }
   return retVal;
 }
  
 function FntIdHex2Dec(value){
-  var retVal = parseInt(value,16);
+  let retVal = parseInt(value, 16);
   if (!retVal){
     retVal = 0;
   }
@@ -90,13 +91,13 @@ function getValue(obj,elements){
   elements.forEach(element => {
     var e = document.getElementById(element);
     //console.log("type=" + e.tagName);
-    if (e.tagName == 'select-one'){
+    if (e.tagName === 'select-one'){
       //console.log("select " + element);
       obj[element] = NUMBER(e.checked);
-    }else if (e.getAttribute('type') == 'checkbox') {
+    }else if (e.getAttribute('type') === 'checkbox') {
       //console.log("checkbox " + element);  
       obj[element] = Number(e.checked);
-    } else if (e.getAttribute('type') == 'number'){
+    } else if (e.getAttribute('type') === 'number'){
       //console.log("number " + element);  
       obj[element] = Number(e.value);
     } else {
@@ -107,14 +108,11 @@ function getValue(obj,elements){
   return obj;
 }
 
-
-
 function doSendData(elements, save) {
-  var obj = { save: save };
+  let obj = {save: save};
   getValue(obj,elements);
   doSend(JSON.stringify(obj));
 }
-
 
 function noType(element) {
     return element;
